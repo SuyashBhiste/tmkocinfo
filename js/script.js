4e8108ae2285b64932cd5849030d2142059a51fb
Suyash Bhiste <suyashbhiste0@gmail.com>
2020-12-20 16:15:10 +0530

    Uploading TMKOC Info Project files in repo

---
+++ js/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+	$('.parallax').parallax();
+	$('.slider').slider();
+});
+
+setTimeout(function(){
+   $('.hide-video').hide();
+   $('.hide-image').show();
+}, 6000);
